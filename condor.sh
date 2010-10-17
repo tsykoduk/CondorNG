a8d19b03d59b5553dcd869443bcb44ed0176d850
Greg Nokes <Greg@nokes.name>
2010-10-17 14:00:23 -0700

    cleanup, fixup, working product

--- condor.sh
+++ condor.sh
@@ -11,18 +11,23 @@
 #3.2 10/31/05 Need to follow one email directive 
 #3.3 11/02/05 Need to have email sent only after host down on second cycle; added safteynet 
 #3.3.1 11/03/05 Added MAILGATE, changed all paths to belfry. Moved exectution to belfry. 
-#3.4 9/16/10 Started to use BASE to set the base directory to local. Change BASE to change execution directory
+#3.4 9/16/10 Moved execution to the directory where CondorNG is
 
 ########### # Defines # ########### 
-BASE=. 
-TEMP=$BASE/tmp/condor.tmp 
-LOG=$BASE/log/condor.log
-TRACK=$BASE/log/condordown.log 
-SITE=`cat $(BASE)/site.cfg` 
-TOAST=$BASE/tmp/condor.toast 
-ML=`cat $(BASE)/mail.cfg` 
-RPT=$BASE/tmp/condor.rpt 
-MAILGATE= 
+TEMP=./tmp/condor.tmp
+TEMPDIR=./tmp/ 
+LOG=./log/condor.log
+TRACK=./log/condordown.log 
+SITE=`cat ./site.cfg` 
+TOAST=./tmp/condor.toast 
+ML=`cat ./mail.cfg` 
+RPT=./tmp/condor.rpt 
+MAILGATE="localhost"
+
+##NOTES:
+#Everything should point to the directory where you put condor.
+#You'll want to change mailgate if you want to use email
+
 
 ############# # Functions # ############# 
 # All of my functions go here 
@@ -30,45 +35,50 @@ PING () {
 # Do Pinging and write to Logfile 
 # Also check for previous down state, ignore if site was up 
 # callhome with down if site down, callhome with up if site came back up  
-for i in $SITE do 
-	if ! ping -c 3 -w 5 $i>/dev/null then 
+for i in $SITE 
+do 
+	if ! ping -c 3 -i 5 $i>$TEMP; then 
 		echo $i:noping 
 		printf "Host:\tDown\t$i\t$(date)\n">>$LOG 
-		if [ -e "/tmp/$i.ping.dropped" ] 
-		then 
+		if [ -e "$TEMPDIR$i.ping.dropped" ]; then 
 			echo $i:down 
 			CALLHOME $i Down 
-			rm /tmp/$i.ping* 
-			touch /tmp/$i.ping.no 
+			rm $TEMPDIR/$i.ping* 
+			touch $TEMPDIR/$i.ping.no 
 			printf "Host:\tDown\t$i\t$(date)\n">>$TRACK 
-		elif [ -e "/tmp/$i.ping.yes" ] 
-		then 
-			echo $i:dropped rm /tmp/$i.ping* 
-			touch /tmp/$i.ping.drop 
+		elif [ -e "$TEMPDIR$i.ping.yes" ]; then 
+			echo $i:dropped rm $TEMPDIR/$i.ping* 
+			touch $TEMPDIR/$i.ping.drop 
 			printf "Host:\tDropped\t$i\t$(date)\n">>$TRACK 
 		else 
-			echo $i:dropped rm /tmp/$i.ping* 
-			touch /tmp/$i.ping.drop 
+			echo $i:dropped rm $TEMPDIR/$i.ping* 
+			touch $TEMPDIR/$i.ping.drop 
 			printf "Host:\tDropped\t$i\t$(date)\n">>$TRACK 
 		fi 
 	else 
-		if [ -e "/tmp/$i.ping.no" ] 
-		then 
+		if [ -e "$TEMPDIR$i.ping.no" ]; then 
 			echo $i:back CALLHOME $i Up 
 			printf "Host:\tUp\t$i\t$(date)\n">>$TRACK 
 			printf "Host:\tUp\t$i\t$(date)\n">>$LOG 
-		elif [ -e "/tmp/$i.ping.drop" ] 
-		then 
+		elif [ -e "$TEMPDIR$i.ping.drop" ]; then 
 			echo $i:back 
 			printf "Host:\tUp\t$i\t$(date)\n">>$LOG 
 			printf "Host:\tUp\t$i\t$(date)\n">>$TRACK
-		elif [ -e "/tmp/$i.ping.yes" ] 
-		then 
+		elif [ -e "$TEMPDIR$i.ping.yes" ]; then 
 			printf "Host:\t\t$i\t$(date)\n">>$LOG 
 		fi 
 		echo $i:pingcheck 
-		rm /tmp/$i.ping* 
-		touch /tmp/$i.ping.yes 
+		rm $TEMPDIR/$i.ping* 
+		touch $TEMPDIR/$i.ping.yes
+		# There might be something that you want to test
+		# Since we know the machine is up, you can put your specific tests here
+		#for example:
+		# if wget $i then
+		# 	printf "Host:\tWWW Up\t$i\t$(date)\n">>$LOG
+		# else
+		#	printf "Host:\tWWW Down\t$i\t$(date)\n">>$LOG
+		# fi
+		#
 	fi 
 done 
 }  
@@ -80,9 +90,10 @@ printf "$1 $2\n">> $RPT
 
 MAILER () { 
 # Check to see if we can get to the gateway 
-if ping -c 6 -w 10 $MAILGATE>/dev/null then 
+if ping -c 3 -i 5 $MAILGATE>$TEMP; then 
 	#Yup - proceed 
-	for m in $ML do cat $RPT | mail -s "Condor Report" $m 
+	for m in $ML; do 
+		cat $RPT | mail -s "Condor Report" $m 
 	done
 else 
 	# Uh-oh. We have a problem! 
@@ -97,20 +108,28 @@ fi
 MCP () {  
 # Master Control Program  
 # Initial housekeeping 
-mv $TEMP /tmp/condor2.tmp touch $TEMP>/dev/null 
+mv $TEMP $TEMPDIR/condor2.tmp 
+touch $TEMP>/dev/null 
+
 # We need to create the file, so the delete does not throw an error 
-touch $RPT rm $RPT>/dev/null  
+touch $RPT 
+rm $RPT>/dev/null  
+
 # Write cool line to log 
 echo " =========================================================================">> $LOG  
+
 # Run Ping Checks on all target hosts 
 PING  
+
 # Did we have a major problem last cycle? 
-if [ -e "$TOAST" ] then 
+if [ -e "$TOAST" ]; then 
 	printf "ALERT ALERT ALERT ALERT ALERT $(date) Main Site BACK!!\n">>$RPT 
 	rm $TOAST 
 fi  
+
 # If we have something to report, then send it off 
-if [ -e "$RPT" ] then MAILER 
+if [ -e "$RPT" ]; then 
+	MAILER 
 fi 
 } 
 
